88743bf052640f0a27a036d6e68e23b7570afd43
Carter Temm <crtbraille@gmail.com>
2017-08-09 01:24:00 -0500

    adds wrapping, sounds, and the ability to speak position info to the menu

--- AGK/misc/menu.py
+++ AGK/misc/menu.py
@@ -1,3 +1,4 @@
+from ..audio import sound
 from ..speech import SAPI, auto
 from ..mainframe import keyboard
 import pygame
@@ -8,40 +9,96 @@ class menu_item(object):
 		self.is_tts=is_tts
 
 class menu(object):
-	def __init__(self, sapi=False):
+	def __init__(self, sapi=False, home_and_end=True, wrap=False, select_with_enter=True, select_with_space=False, announce_position_info=False):
 		self.position=0
 		self.items=[]
 		self.sapi=sapi
+		self.home_and_end=home_and_end
+		self.wrap=wrap
+		self.item_sound=""
+		self.enter_sound=""
+		self.select_with_enter=select_with_enter
+		self.select_with_space=select_with_space
+		self.announce_position_info=announce_position_info
 
 	def add_item_tts(self,text,name):
 		self.items.append(menu_item(text,name,True))
 
-	def run(self,intro):
-		self.speak(intro)
-		self.position=-1
+	def run(self,intro=None,is_intro_tts=True,starting_position=-1):
+		if self.enter_sound:
+			self.entersnd=sound.sound()
+			self.entersnd.load(self.enter_sound)
+		if self.item_sound:
+			self.itemsnd=sound.sound()
+			self.itemsnd.load(self.item_sound)
+		if intro:
+			if is_intro_tts:
+				self._speak(intro)
+			else:
+				s=sound.sound()
+				s.load(intro)
+				s.play()
+		self.position=starting_position
 
 		while 1:
 			for event in pygame.event.get():
 				if event.type == pygame.KEYDOWN:
 					if event.key==pygame.K_UP:
 						if self.position<=0:
-							self.position=0
+							if self.wrap:
+								self.position=len(self.items)-1
+							else:
+								self.position=0
 						else:
 							self.position-=1
-						self.speak(self.items[self.position].text)
-					if event.key==pygame.K_DOWN:
+						if self.announce_position_info:
+							self._speak(self.items[self.position].text+". "+str(self.position+1)+" of "+str(len(self.items)))
+						else:
+							self._speak(self.items[self.position].text)
+						if self.item_sound:
+							self.itemsnd.stop()
+							self.itemsnd.play()
+					elif event.key==pygame.K_DOWN:
 						if self.position<len(self.items)-1:
 							self.position+=1
-						self.speak(self.items[self.position].text)
+						elif self.position>=len(self.items)-1 and self.wrap:
+							self.position=0
+						if self.announce_position_info:
+							self._speak(self.items[self.position].text+". "+str(self.position+1)+" of "+str(len(self.items)))
+						else:
+							self._speak(self.items[self.position].text)
+						if self.item_sound:
+							self.itemsnd.stop()
+							self.itemsnd.play()
+					elif event.key==pygame.K_HOME:
+						self.position=0
+						if self.announce_position_info:
+							self._speak(self.items[self.position].text+". "+str(self.position+1)+" of "+str(len(self.items)))
+						else:
+							self._speak(self.items[self.position].text)
+						if self.item_sound:
+							self.itemsnd.stop()
+							self.itemsnd.play()
+					elif event.key==pygame.K_END:
+						self.position=len(self.items)-1
+						if self.announce_position_info:
+							self._speak(self.items[self.position].text+". "+str(self.position+1)+" of "+str(len(self.items)))
+						else:
+							self._speak(self.items[self.position].text)
+						if self.item_sound:
+							self.itemsnd.stop()
+							self.itemsnd.play()
 
-					if event.key==pygame.K_RETURN:
+					if event.key==pygame.K_RETURN and self.select_with_enter or event.key==pygame.K_SPACE and self.select_with_space:
+						if self.enter_sound:
+							self.entersnd.play_wait()
 						return self.items[self.position]
 
 					if event.key==pygame.K_ESCAPE:
 						return -1
 
 #internal funcs
-	def speak(self,text):
+	def _speak(self,text):
 		if self.sapi==False:
 			auto.speak(text)
 		else:
